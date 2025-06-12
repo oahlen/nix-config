329c45543fec5d027fe1135a70d15efdc17c262b
Oscar Ahlén <github@ahlen.dev>
2025-06-12 12:38:54 +0200

    Update gnome config for 25.11

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -9,9 +9,8 @@ with lib; {
     "${nixos-modules}/desktop/shared/fonts.nix"
   ];
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   environment.systemPackages = with pkgs;
     [
