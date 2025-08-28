a4528f6eadb49a8cc90b5a55e38d1aa00eca06c9
Oscar Ahlén <github@ahlen.dev>
2025-08-28 18:37:30 +0200

    Enable 32 bit graphics

--- modules/nixos/profiles/gaming/default.nix
+++ modules/nixos/profiles/gaming/default.nix
@@ -3,4 +3,9 @@
     "${nixos-modules}/programs/corectrl"
     "${nixos-modules}/programs/gamemode"
   ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; # Required by some more modern games like The Witcher 3
+  };
 }
