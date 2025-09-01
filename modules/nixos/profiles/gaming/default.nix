e22f35fbd2c396d282bd188648f689814df5aa29
Oscar Ahlén <github@ahlen.dev>
2025-09-01 15:38:44 +0200

    Add improved gamepad support

--- modules/nixos/profiles/gaming/default.nix
+++ modules/nixos/profiles/gaming/default.nix
@@ -1,11 +1,24 @@
-{nixos-modules, ...}: {
+{
+  nixos-modules,
+  pkgs,
+  ...
+}: {
   imports = [
     "${nixos-modules}/programs/corectrl"
     "${nixos-modules}/programs/gamemode"
   ];
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true; # Required by some more modern games like The Witcher 3
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true; # Required by some more modern games like The Witcher 3
+    };
+
+    uinput.enable = true; # Virtual gamepad support
   };
+
+  # Add udev rules for common game controllers
+  services.udev.packages = with pkgs; [
+    game-devices-udev-rules
+  ];
 }
