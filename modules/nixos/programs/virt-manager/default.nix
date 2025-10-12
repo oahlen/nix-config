8356c55b8acf4913323f59c5e5860959c6e946d6
Oscar Ahlén <github@ahlen.dev>
2025-10-12 14:03:48 +0200

    Remove deprecated setting

--- modules/nixos/programs/virt-manager/default.nix
+++ modules/nixos/programs/virt-manager/default.nix
@@ -3,10 +3,8 @@
   user,
   ...
 }: {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu.ovmf.enable = true;
-  };
+  virtualisation.libvirtd.enable = true;
+
   programs.dconf.enable = true;
 
   environment.systemPackages = with pkgs; [
