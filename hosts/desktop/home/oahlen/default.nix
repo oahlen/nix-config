a99f1f51122fb1cd9fd61d13476398c69ab506b2
Oscar Ahlén <github@ahlen.dev>
2025-10-13 10:06:35 +0200

    Remove sway autostart

--- hosts/desktop/home/oahlen/default.nix
+++ hosts/desktop/home/oahlen/default.nix
@@ -6,13 +6,6 @@
     "${hm-modules}/programs/rbw"
   ];
 
-  programs.bash.profileExtra = ''
-    # If running from tty1 start sway
-    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-        exec sway
-    fi
-  '';
-
   programs = {
     spotify-player.enable = true;
   };
