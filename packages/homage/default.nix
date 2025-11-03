6b900b29dbbee25fa2881967240fe1028bf3194f
Oscar Ahlén <github@ahlen.dev>
2025-11-03 20:57:18 +0100

    Bump homage

--- packages/homage/default.nix
+++ packages/homage/default.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "homage";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "oahlen";
     repo = "homage";
     rev = version;
-    sha256 = "sha256-yqGzNAlYDnykBSy3k+iDzyGdn4ff47Oa21+VxFGl1zg=";
+    sha256 = "sha256-pxrfC9SfMVopPjTvV9IrvtRzwo3CISrPaPQNiGc8vAE=";
   };
 
-  cargoHash = "sha256-VgSQKgr+jCgQHC4PFVQiS3zIawXyuOtCPal5fs97i2k=";
+  cargoHash = "sha256-JKRLdCEILCcOdygjC1k/SJ+MWjzkZlYoWA0E68JT+7E=";
 
   meta = with lib; {
     description = "Simple and effective dotfiles manager for your home.";
