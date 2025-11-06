8e4fffc37bafe445f9410624a3a3bbfe7b686cc3
Oscar Ahlén <github@ahlen.dev>
2025-11-06 16:54:40 +0100

    Fix rebuild command

--- packages/rebuild/default.nix
+++ packages/rebuild/default.nix
@@ -7,12 +7,12 @@ writeShellApplication {
   runtimeInputs = [ nixos-rebuild-ng ];
   text = ''
     if [[ $# -eq 0 ]]; then
-        nixos-rebuild --help
+        nh os --help
         exit 0
     fi
 
     HOSTNAME="$(hostname)"
     echo -e "Running nixos-rebuild \e[1;33m$1\e[0m for host \e[1;36m$HOSTNAME\e[0m ..."
-    ng os "$1" --file . -A "hosts.$HOSTNAME" --no-nom
+    nh os "$1" --file . "hosts.$HOSTNAME" --no-nom
   '';
 }
